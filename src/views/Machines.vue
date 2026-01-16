6fae1696374afcf4d93adebc1779562e658d5440
Bawantha Dilshan <s22010683@ousl.lk>
2026-01-16 20:17:27 +0530

    Initial commit

---
+++ src/views/Machines.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="machines">
+    <h2>Machines</h2>
+    <p>List and manage machines here.</p>
+  </section>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.machines { padding: 1rem; }
+</style>
