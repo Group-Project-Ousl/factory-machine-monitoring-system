825f873e5e0668957e0ceffcb87a98614156a430
Bawantha Dilshan <s22010683@ousl.lk>
2026-03-10 10:39:25 +0530

    update

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,7 +8,7 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: '/dashboard'
+        redirect: '/login'
       },
       {
         path: 'dashboard',
